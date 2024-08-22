d7038d97fe4f3cb6f3614ec390d362aed2b6111f
obhim <128315719+obhim@users.noreply.github.com>
2024-08-22 23:19:54 +0530

    Add files via upload
    first commit

---
+++ vimca/src/main/java/com/monkey/monkey/VimcaApplication.java
@@ -0,0 +1,13 @@
+package com.monkey.monkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimcaApplication.class, args);
+	}
+
+}
